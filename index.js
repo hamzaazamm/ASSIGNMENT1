eef260cc98f2b5624491ce7dda5d2fa3901c4091
Hamza <111911421+hamzaazamm@users.noreply.github.com>
2022-08-29 17:29:23 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,55 @@
+console.log("Hello World");
+console.log(10+8);
+
+
+console.log('Hamza');
+console.log(4);
+console.log(true);
+console.log(null); ;
+console.log([1, 2, 3]);
+console.log({name:"hamza", language:"solidity", tutorial:6});
+
+
+console.table({name: 'hamza', language:javascript});
+
+
+console.warn('warning');
+
+
+console.time();
+for (i = 0; i < 100; i++) {
+}
+console.timeEnd();
+
+
+
+document.write("Hello Word ");
+document.write(2+8);
+
+
+var nationality = "Pakistani";
+var age = 15;
+var isFeePaid = true;
+var weight = 60.55;
+
+
+var age; // Declaration
+age = 25; // Initialization
+
+
+let age = 25;
+let name = "hamza"; 
+let choice = true;// Boolean
+let location = null; 
+let task; 
+let age= 25;
+console.log(age); 
+
+
+
+
+const age = 200;
+const job = 'developer';
+const name; 
+const num = 20;
+num = 30; 
